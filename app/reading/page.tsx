5a04f3a40ffb5cd8730d9a7376529b45673f86d4
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-06 00:50:45 +0100

    added structure and layout to /reading page, with styling for pc and mobile

--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -1,9 +1,52 @@
 import { Input } from "@/components/ui/input";
+import { Celtic } from "@/components/ui/reading/celtic-cross";
+import Image from "next/image";
 
 export default function Reading() {
   return (
     <>
-      <Input />
+      <div className="grid grid-rows-[1px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
+          <div className="px-12 flex flex-row items-end justify-end">
+            <Image
+              src="/celtic.png"
+              alt="Tarot Reader logo"
+              width={160}
+              height={38}
+              priority
+              className="mr-4"
+            />
+            <span className="px-[10px] sm:pt-0 text-[36px] sm:text-[50px] font-[family-name:var(--font-geist-mono)]">
+              Ask <br />a Question...
+            </span>
+          </div>
+          <section className="w-full sm:w-[80vw] px-12 pt-6 flex flex-row gap-2">
+            <Input />
+            <button className="w-9 h-9 border border-input rounded-md flex justify-center items-center text-sm hover:brightness-150">
+              ⎆
+            </button>
+            <div className="w-9 h-9 border border-input rounded-md flex justify-center items-center text-sm hover:brightness-150">
+              10
+            </div>
+          </section>
+          <section className="w-full px-12 sm:w-[80vw] flex flex-row justify-center sm:flex-row sm:justify-start pt-12">
+            <Celtic />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
+
+const tarotCards = [
+  { description: "The Fool" },
+  { description: "The Magician" },
+  { description: "The High Priestess" },
+  { description: "The Empress" },
+  { description: "The Emperor" },
+  { description: "The Hierophant" },
+  { description: "The Lovers" },
+  { description: "The Chariot" },
+  { description: "Strength" },
+  { description: "The Hermit" },
+];
---
+++ components/ui/reading/celtic-cross.tsx
@@ -0,0 +1,37 @@
+const TarotCard = ({ position, label }) => (
+  <div className="relative">
+    <div className="w-24 h-32 flex items-center justify-center bg-slate-200 border-2 rounded-lg shadow-md hover:bg-slate-300 transition-colors cursor-pointer">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <span className="text-sm text-slate-600">{position}</span>
+      </div>
+    </div>
+    <div className="mt-[2px] text-xs text-center text-slate-600 max-w-24">
+      {label}
+    </div>
+  </div>
+);
+
+export const Celtic = () => {
+  return (
+    <main className="flex flex-row">
+      <section className="flex flex-col pr-12 gap-2">
+        <TarotCard position={1} label={"ee"} />
+        <TarotCard position={1} label={"ee"} />
+        <TarotCard position={1} label={"ee"} />
+        <TarotCard position={1} label={"ee"} />
+      </section>
+      <section className="flex flex-col justify-center">
+        <TarotCard position={1} label={"ee"} />
+      </section>
+      <section className="flex flex-col justify-evenly sm:px-12">
+        <TarotCard position={1} label={"ee"} />
+        <TarotCard position={1} label={"ee"} />
+        <TarotCard position={1} label={"ee"} />
+      </section>
+
+      <section className="flex flex-col justify-center">
+        <TarotCard position={1} label={"ee"} />
+      </section>
+    </main>
+  );
+};
