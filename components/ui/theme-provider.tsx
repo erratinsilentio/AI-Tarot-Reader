8f49868897241dee3f0ba4aea490cf8b24204e8f
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-07 11:25:20 +0100

    one more ts ignore

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,7 +1,8 @@
 "use client";
-
+// @ts-ignore
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+// @ts-ignore
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
