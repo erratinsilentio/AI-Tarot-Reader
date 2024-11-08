eedc9eb34c0c49e37f917b8f96ea248bb7a96064
erratinsilentio <kacper.kondracki@wp.pl>
2024-11-08 11:05:05 +0100

    started working on /learn page

--- app/page.tsx
+++ app/page.tsx
@@ -41,14 +41,13 @@ export default async function Home() {
               </span>
               <span className="pr-[8px]"> Start Your Reading</span>
             </Link>
-            <a
+            <Link
               className="w-[200px] sm:w-auto rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
+              href="/learn"
               rel="noopener noreferrer"
             >
               Read more
-            </a>
+            </Link>
           </div>
           <AlertComponent />
         </main>
